b13a246f9c062c705268136f2f86563f3ee89d8b
Ujjwal Goyal <ujjwal.goyal@bounceshare.com>
2020-03-05 16:54:27 +0530

    Updates

---
+++ precache-manifest.4afd90a2ab25b7721d3e9c1c0b8d75f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9acdcfd913194186acb9030efb4de14a",
+    "url": "/contactme/index.html"
+  },
+  {
+    "revision": "7f42138b21101b21e814",
+    "url": "/contactme/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "523120346718c357b8b6",
+    "url": "/contactme/static/js/2.42ebdd7e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/contactme/static/js/2.42ebdd7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f42138b21101b21e814",
+    "url": "/contactme/static/js/main.07ae0937.chunk.js"
+  },
+  {
+    "revision": "17dfde3b3a78983693ff",
+    "url": "/contactme/static/js/runtime-main.231746d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/contactme/static/media/logo.5d5d9eef.svg"
+  }
+]);
